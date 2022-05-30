21060528b3bb3d3af68db33ab6ba5086d0025e09
Tăng Quốc Tuấn <tuantq1911@gmail.com>
2022-05-30 16:54:24 +0700

    Add files via upload
    init alias config

---
+++ alias.ps1
@@ -0,0 +1,11 @@
+#open $profile with nodepad
+Function op{ notepad $PROFILE }
+
+# --------- yarn ---------
+Function yi{ yarn install }
+Function ys{ yarn start }
+Function yt{ yarn test }
+Function yb{ yarn build }
+Function yd{ yarn dev }
+
+Function cl{ clear }
